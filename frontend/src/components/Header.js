2f789e79cc70c8ab4a385a7c1b65ec5b034fa920
darklegend99 <darklegend1931@gmail.com>
2022-09-28 16:47:36 +0530

    blog website

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import {AppBar,Box,Button,Tab,Tabs,Toolbar, Typography} from '@mui/material'
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { authActions } from '../store';
+
+const Header = () => {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state)=>state.isLoggedIn)
+  const [value, setValue] = useState();
+
+  return <AppBar position='sticky' sx={{background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(96,177,254,1) 100%);"}}>
+    <Toolbar>
+      <Typography variant="h4">BlogsApp</Typography>
+      { isLoggedIn && <Box display='flex' marginLeft={'auto'} marginRight={'auto'}>
+        <Tabs textColor='inherit' value={value} onChange={(e, val)=>setValue(val)}> 
+          <Tab LinkComponent={Link} to="/blogs" label="All Blogs"></Tab>
+          <Tab LinkComponent={Link} to="/myBlogs" label="My Blogs"></Tab>
+          <Tab LinkComponent={Link} to="/blogs/add" label="Add Blog"></Tab>
+        </Tabs>
+      </Box> }
+      <Box display="flex" marginLeft="auto">
+        { !isLoggedIn && <><Button LinkComponent={Link} to="/auth" variant='contained' sx={{margin: 1, borderRadius: 10}} color='warning'>Login</Button>
+        <Button LinkComponent={Link} to="/auth" variant='contained' sx={{margin: 1, borderRadius: 10}} color='warning'>Signup</Button> </> }
+        { isLoggedIn && <Button onClick={()=>dispatch(authActions.logout())} LinkComponent={Link} to="/auth" variant='contained' sx={{margin: 1, borderRadius: 10}} color='warning'>Logout</Button> }
+      </Box>
+    </Toolbar>
+     </AppBar>;
+}
+
+export default Header
